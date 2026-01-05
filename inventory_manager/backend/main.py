d2cd626626680e5e02179517ad93a230f7f0c915
James Hall <ii.jameseh@gmail.com>
2026-01-05 09:18:14 -0500

    add code to run server in main.py/backend. fixed pydantic issue.

--- inventory_manager/backend/main.py
+++ inventory_manager/backend/main.py
@@ -36,3 +36,7 @@ app.include_router(items.router)
 app.include_router(upload.router)
 app.include_router(transactions.router)
 app.include_router(dashboard.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
